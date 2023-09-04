71ff7bf2533f174912048813dc2fa7680ff9f009
Alice Everglade <AliceEverglade@gmail.com>
2023-09-04 09:07:56 +0200

    GPT connection is working, just no tokens

---
+++ AicoAssistant/AicoAssistant/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AicoAssistant
+{
+    public static class Constants
+    {
+        public const string Key = "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";
+    }
+}
---
+++ AicoAssistant/AicoAssistant/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace AicoAssistant
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = null;
+            bool running = false;
+            OpenAiApiClient client = null;
+
+            key = Constants.Key;
+            if (key == null)
+            {
+                Console.WriteLine("I do not have a key to connect to the mainframe. please type out your key and press enter.");
+                key = Console.ReadLine();
+            }
+
+
+            while (client == null)
+            {
+                APIConnection connection = new APIConnection(key);
+                client = connection.Connect();
+                if (client != null)
+                {
+                    Console.WriteLine("connected to the mainframe :D ask ahead!");
+                    running = true;
+                }
+                if (client == null)
+                {
+                    Console.WriteLine("the connection failed. maybe the key was wrong? try again please.");
+                    key = Console.ReadLine();
+                }
+            }
+
+            while (running)
+            {
+                string prompt = Console.ReadLine();
+                if (prompt != null)
+                {
+                    SendPrompt(prompt, client);
+                }
+            }
+        }
+
+        static void SendPrompt(string prompt, OpenAiApiClient client)
+        {
+            string generatedText = client.GenerateTextAsync(prompt).GetAwaiter().GetResult();
+            Console.WriteLine(generatedText);
+        }
+    }
+}
